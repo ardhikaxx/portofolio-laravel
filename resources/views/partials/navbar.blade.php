8d2b292b7957b638ee555568da1b0e8fc87c0be4
ardhikaxx <ardhikayanuar58@gmail.com>
2024-04-06 01:58:23 +0700

    update

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav id="header">
+    <div class="nav-logo">
+        <p class="nav-name">ARDHIKA</p>
+    </div>
+    <div class="nav-menu" id="myNavMenu">
+        <ul class="nav_menu_list">
+            <li class="nav_list">
+                <a href="#home" class="nav-link active-link">Dashboard</a>
+                <div class="circle"></div>
+            </li>
+            <li class="nav_list">
+                <a href="#about" class="nav-link">About</a>
+                <div class="circle"></div>
+            </li>
+            <li class="nav_list">
+                <a href="#projects" class="nav-link">Projects</a>
+                <div class="circle"></div>
+            </li>
+            <li class="nav_list">
+                <a href="#skill" class="nav-link">Skill</a>
+                <div class="circle"></div>
+            </li>
+        </ul>
+    </div>
+    <div class="nav-button">
+        <a href="./html/certificate.html" class="btn">Certificate <i class="fas fa-solid fa-award"></i></a>
+    </div>
+    <div class="nav-menu-btn">
+        <i class="uil uil-bars" onclick="myMenuFunction()"></i>
+    </div>
+</nav>
